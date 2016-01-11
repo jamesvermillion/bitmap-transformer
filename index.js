4a4f4773ea6fffe53329428442bee3e5cd2d944e
jamesvermillion <jamesvermillion@yahoo.com>
2016-01-11 09:24:01 -0800

    modularized and working, needs test and linting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const BDR = require('./lib/bitmap_data_reader.js');
 const EE = require('events').EventEmitter; 
 const transformer = require('./lib/transform');
 const processOrig = BDR.meta;
-
+const transforms = ['red', 'gray', 'invert'];
 var ee = new EE();
 var bitmapObject = {};
 
@@ -10,11 +10,14 @@ if (process.argv.length < 2 || !process.argv[2].endsWith('.bmp') && !process.arg
   console.log('please enter bitmap file as third argument');
   return;
 }
-
+if (process.argv.length < 4 || transforms.indexOf(process.argv[3].toLowerCase()) === -1 ){
+  console.log('invert will be your transform');
+  bitmapObject.transform = 'invert'; 
+} else {
+  console.log(`${process.argv[3]} will be your transform`);
+  bitmapObject.transform = process.argv[3]; 
+}
 start = function () {
-  // path property
   bitmapObject.path = process.argv[2];
-  // transform property
-  bitmapObject.transform = process.argv[3] || 'invert';
   processOrig(bitmapObject);
 }();
